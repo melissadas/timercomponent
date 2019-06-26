4736a2cbbf673987bc03c55b367fb9e8ca362448
melissadas <melissadas22@gmail.com>
2019-06-26 17:50:09 +0200

    First commit Timer component

---
+++ src/TimerComponent.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Button, Row, Container, Col } from 'react-bootstrap';
+
+class TimerComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentTime: 0,
+            startTime: 0,
+            showButton: false
+
+        }
+
+
+    }
+
+    startTimer = () => {
+        this.setState({
+            startTime: Date.now() - this.state.currentTime,
+            currentTime: this.state.currentTime,
+            showButton: true
+        })
+        console.log("Start timer");
+        this.timer = setInterval(() => this.setState({
+            currentTime: Date.now() - this.state.startTime
+        }), 1)
+    }
+
+
+
+    stopTimer = () => {
+        clearInterval(this.timer);
+    }
+
+    pauseTimer() {
+
+    }
+
+    resetTimer = () => {
+        this.setState({
+            currentTime: 0
+        })
+    }
+
+
+    render() {
+
+
+
+        return (
+            <Container>
+                <Row>
+
+                    <Col><p>Displaying the current time here:</p>
+                        <div>{Date(this.state.currentTime).toString()}</div></Col>
+                    <Col>
+
+                        <Button variant="success" onClick={this.startTimer} >Start</Button></Col>
+                    <Col> <Button variant="danger" onClick={this.stopTimer} disabled={!this.state.showButton}>Stop</Button></Col>
+                    <Col><Button variant="warning" onClick={this.pauseTimer} disabled={!this.state.showButton}>Pause</Button></Col>
+                    <Col> <Button variant="info" onClick={this.resetTimer}>Reset</Button></Col>
+                </Row>
+
+                <Row>
+
+                </Row>
+
+            </Container>
+        )
+    }
+}
+
+export default TimerComponent;
